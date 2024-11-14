ba959d08330805a1fc85e443f6e806228b7005d6
MoroMattia02 <mattimoro2002@gmail.com>
2024-11-14 21:42:06 +0100

    Update data_utils.py

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -18,3 +18,14 @@ def clean_na_columns(df, threshold = 0.5):
     
     return df.dropna(axis = 1, thresh = threshold)
 
+
+def nan_to_numeric(df):
+    # Select columns that contain NaNs
+    columns_with_nans_nn = df.columns[df.isna().any()]
+
+    # Convert columns with NaNs to np.float64 if they contain only numeric values or NaNs
+    for col in columns_with_nans_nn:
+        if df[col].apply(lambda x: isinstance(x, (int, float)) or pd.isna(x)).all():
+            # Convert column to np.float64
+            df[col] = df[col].astype(np.float64)
+    return df
