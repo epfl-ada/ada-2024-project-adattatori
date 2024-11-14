6c18e269e0de31e805a7d41935cb6d549cdbda27
MoroMattia02 <mattimoro2002@gmail.com>
2024-11-14 10:22:22 +0100

    Update some_dataloader.py

--- src/data/some_dataloader.py
+++ src/data/some_dataloader.py
@@ -1,24 +1,30 @@
-import torch
 import numpy as np
-from torch.utils.data import DataLoader, Dataset
+import os
+import pandas as pd
+import zipfile
 
 
-class SomeDataset(Dataset):
-    """
-    A dataset implements 2 functions
-        - __len__  (returns the number of samples in our dataset)
-        - __getitem__ (returns a sample from the dataset at the given index idx)
-    """
-
-    def __init__(self, dataset_parameters, **kwargs):
-        super().__init__()
-        ...
+def find_repo_root():
+    currpath = os.path.dirname(os.path.abspath(__file__))
+    while currpath != os.path.dirname(currpath):  # As long as we dont reach the right root
+        if os.path.exists(os.path.join(currpath, '.git')):
+            return currpath
+        currpath = os.path.dirname(currpath)
+    raise Exception("repo root not found")
 
 
-class SomeDatamodule(DataLoader):
-     """
-    Allows you to sample train/val/test data, to later do training with models.
-        
-    """
-    def __init__(self):
-        super().__init__()
+def read_tsv_from_zip(zip_path, file_name_in_zip):
+    # Unzips the file and reads the TSV
+    with zipfile.ZipFile(zip_path, 'r') as zip_ref:
+        with zip_ref.open(file_name_in_zip) as tsv_file:
+            # Read the file directly as a TSV
+            df = pd.read_csv(tsv_file, sep = '\t', low_memory = False)
+    return df
+
+def get_dataset():
+    root = find_repo_root()
+    path_to_data = os.path.join(root,'data','BindingSTD.zip')
+    filename = 'BindingSTD.tsv'
+    df = read_tsv_from_zip(path_to_data, filename)
+    print(df.head())
+    return df
