61b4daaced0b2ebff863e6f5edc73049bb02d414
MoroMattia02 <mattimoro2002@gmail.com>
2024-11-14 22:09:47 +0100

    Results update

--- src/utils/data_utils.py
+++ src/utils/data_utils.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import requests
 
 def select_metric(df, metric = 'IC50 (nM)'):
     df[metric] = pd.to_numeric(df[metric], errors = 'coerce')
@@ -29,3 +30,23 @@ def nan_to_numeric(df):
             # Convert column to np.float64
             df[col] = df[col].astype(np.float64)
     return df
+
+# function to do an http request to get the publication year using CrossRef
+def get_publication_year(doi):
+    # format the URL to include the DOI in the request
+    url = f"https://api.crossref.org/works/{doi}"
+    try:
+        ##send an HTTP GET request
+        response = requests.get(url)
+        #raise an error for bad responses
+        response.raise_for_status()  
+        ### parse the JSON response
+        data = response.json()
+        
+        ### extract the publication year from the response data
+        year = data['message']['published']['date-parts'][0][0]
+        return year
+    except Exception as e:
+        ## print an error message if something goes wrong
+        print(f"Could not retrieve year for DOI {doi}: {e}")
+        return None
